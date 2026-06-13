7d090632dfc41bc8fc6e08e57a654a74eab3bc40
kanghyein75 <kanghye675@gmail.com>
2026-06-13 21:25:52 +0900

    Create student.h

---
+++ student.h
@@ -0,0 +1,15 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+typedef struct Student {
+    int id;
+    char name[32];
+    int score;
+    struct Student *next;
+} Student;
+
+void addStudent(Student **head, int id, const char *name, int score);
+void printStudents(Student *head);
+void freeStudents(Student **head);
+
+#endif

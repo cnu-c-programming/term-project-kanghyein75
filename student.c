6af24dea91c755824d6041557f4de77693347f2c
kanghyein75 <kanghye675@gmail.com>
2026-06-17 02:33:51 +0900

    Update student.c

--- student.c
+++ student.c
@@ -27,6 +27,43 @@ void addStudent(Student **head, int id, const char *name, int score) {
     }
 }
 
+int deleteStudent(Student **head, int id) {
+    if (*head == NULL) return 0;
+
+    Student *temp = *head;
+    
+    if (temp->id == id) {
+        *head = temp->next; 
+        free(temp);         
+        return 1;
+    }
+
+    Student *prev = NULL;
+    while (temp != NULL && temp->id != id) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) return 0;
+
+    prev->next = temp->next;
+    free(temp); 
+    return 1;
+}
+
+int updateStudent(Student *head, int id, int newScore) {
+    Student *current = head;
+    
+    while (current != NULL) {
+        if (current->id == id) {
+            current->score = newScore; 
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0; 
+}
+
 void freeStudents(Student **head) {
     Student *current = *head;
     Student *nextN; 

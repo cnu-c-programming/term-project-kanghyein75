d8e4364c8327af9f9fd40b37627d8a06380aab59
kanghyein75 <kanghye675@gmail.com>
2026-06-16 15:22:44 +0900

    Create command.h

---
+++ command.h
@@ -0,0 +1,34 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include "student.h"
+
+typedef enum {
+    SHELL_OK = 0,
+    SHELL_EXIT,
+    SHELL_ERR_UNKNOWN_COMMAND,
+    SHELL_ERR_INVALID_ARGUMENT,
+    SHELL_ERR_MISSING_ARGUMENT,
+    SHELL_ERR_FILE_OPEN,
+    SHELL_ERR_FILE_WRITE,
+    SHELL_ERR_STUDENT_NOT_FOUND,
+    SHELL_ERR_DUPLICATE_STUDENT,
+    SHELL_ERR_INVALID_SCORE
+} ShellResult;
+
+typedef ShellResult (*CommandHandler)(char* args, Student** head);
+
+typedef struct {
+    const char* name;
+    CommandHandler handler;
+    const char* usage;
+    const char* description;
+} Command;
+
+extern Command commands[];
+extern int num;
+
+ShellResult handle_list(char* args, Student** head);
+ShellResult handle_find(char* args, Student** head);
+
+#endif

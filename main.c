4049aebfbac3473a7dc84db17c27193961e4c2ae
kanghyein75 <kanghye675@gmail.com>
2026-06-16 15:27:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -21,57 +21,7 @@
 /* TODO: Add your own header includes here */
 #include "student.h"  
 #include "file_io.h"  
-/* #include "command.h"  */
-
-typedef enum {
-    SHELL_OK = 0,
-    SHELL_EXIT,
-    SHELL_ERR_UNKNOWN_COMMAND,
-    SHELL_ERR_INVALID_ARGUMENT,
-    SHELL_ERR_MISSING_ARGUMENT,
-    SHELL_ERR_FILE_OPEN,
-    SHELL_ERR_FILE_WRITE,
-    SHELL_ERR_STUDENT_NOT_FOUND,
-    SHELL_ERR_DUPLICATE_STUDENT,
-    SHELL_ERR_INVALID_SCORE
-} ShellResult;
-
-typedef ShellResult (*CommandHandler)(char* args, Student** head);
-
-typedef struct {
-    const char* name;
-    CommandHandler handler;
-    const char* usage;
-    const char* description;
-} Command;
-
-#ifdef ADMIN_MODE
-Command commands[] = {
-    {"save",    NULL, "save",                  "Save students to CSV"},
-    {"reload",  NULL, "reload",                "Reload students from CSV"},
-    {"add",     NULL, "add <id> <name> <score>","Add a student"},
-    {"delete",  NULL, "delete <id>",           "Delete a student"},
-    {"update",  NULL, "update <id> <score>",   "Update student score"},
-    {"find",    NULL, "find <id>",             "Find student"},
-    {"list",    NULL, "list",                  "List students"},
-    {"stats",   NULL, "stats",                 "Show statistics"},
-    {"help",    NULL, "help",                  "Show help"},
-    {"clear",   NULL, "clear",                 "Clear screen"},
-    {"exit",    NULL, "exit",                  "Exit shell"}
-};
-#endif
-
-#ifdef CLIENT_MODE
-Command commands[] = {
-    {"reload",  NULL, "reload",                "Reload students from CSV"},
-    {"find",    NULL, "find <id>",             "Find student"},
-    {"list",    NULL, "list",                  "List students"},
-    {"stats",   NULL, "stats",                 "Show statistics"},
-    {"help",    NULL, "help",                  "Show help"},
-    {"clear",   NULL, "clear",                 "Clear screen"},
-    {"exit",    NULL, "exit",                  "Exit shell"}
-};
-#endif
+#include "command.h"  
 
 /* ---------------------------------------------------------------
  * TODO: Implement the interactive shell loop.
@@ -80,17 +30,17 @@ Command commands[] = {
  *   - Dispatch to the appropriate handler function.
  *   - Loop until the user types "exit" or EOF.
  * --------------------------------------------------------------- */
-void run_shell(const char *csv_path) {
+void run_shell(Student **head, const char *csv_path) {
     char line[500];
 
 #ifdef ADMIN_MODE
-    const char *user = "admin> ";
+    const char *prompt = "admin> ";
 #else
-    const char *user = "client> ";
+    const char *prompt = "client> ";
 #endif
 
     while (1) {
-        printf("%s", user); 
+        printf("%s", prompt); 
         fflush(stdout);     
 
         if (fgets(line, sizeof(line), stdin) == NULL) {
@@ -103,22 +53,30 @@ void run_shell(const char *csv_path) {
         if (cmd == NULL) continue;
         
         if (strcmp(cmd, "exit") == 0) {
-            printf("end\n");
+            printf("Goodbye.\n");
             break;
         }
 
+        char *args = strtok(NULL, "");
+        if (args != NULL) {
+            while (*args == ' ') args++;
+        }
+
         int found = 0;
-        int num = sizeof(commands) / sizeof(commands[0]);
         for (int i = 0; i < num; i++) {
             if (strcmp(cmd, commands[i].name) == 0) {
-                printf("true\n");
+                commands[i].handler(args, head);
                 found = 1;
                 break;
             }
         }
 
         if (!found) {
-            printf("false\n");
+#ifdef CLIENT_MODE
+            printf("Unknown command or permission denied.\n");
+#else
+            printf("Unknown command.\n");
+#endif
         }
     }
 }
@@ -156,14 +114,27 @@ int main(int argc, char *argv[]) {
 
     Student *head = NULL;
     loadStudents(&head, csv_path);
-    printStudents(head);
+
+    int count = 0;
+    Student *current = head;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+
+#ifdef ADMIN_MODE
+    printf("[Admin Program]\n");
+#elif defined(CLIENT_MODE)
+    printf("[Client Program]\n");
+#endif
+    printf("Loaded %d students from %s.\n", count, csv_path);
 
 #ifdef ADMIN_MODE
     /* Admin shell: supports add, delete, update, save, load, sort, list, find, help, exit */
     if (cmd_file) {
         run_command_file(cmd_file, csv_path);
     } else {
-        run_shell(csv_path);
+        run_shell(&head, csv_path);
     }
 
 #elif defined(CLIENT_MODE)
@@ -171,7 +142,7 @@ int main(int argc, char *argv[]) {
     if (cmd_file) {
         run_command_file(cmd_file, csv_path);
     } else {
-        run_shell(csv_path);
+        run_shell(&head, csv_path);
     }
 
 #else
